360af7632b925d4124e3a58d057e0a8ac9699609
JonathanValleA <jvalle@jaumebalmes.net>
2023-01-30 14:01:17 +0100

    Actividad 1 a1 y a2 + README

--- practica1_react_a1/src/App.js
+++ practica1_react_a1/src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+// Importamos el archivo ejercicio1.js
 import ejercicio1 from './components/ejercicio1';
 
 function App() {
   return (
+    // Retornamos la funcion ejercicio1()
     ejercicio1()
   );
 }
---
+++ practica1_react_a2/src/App.js
@@ -0,0 +1,8 @@
+import ejercicio2 from "./components/ejercicio2";
+function App() {
+  return (
+    ejercicio2()
+  );
+}
+
+export default App;
